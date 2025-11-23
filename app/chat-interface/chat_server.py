de522cac29f81f3d0d48701b604e6722a20cdb4b
Jonas Neves <jonas.neves@duke.edu>
2025-11-23 18:56:57 -0500

    Update

--- app/chat-interface/chat_server.py
+++ app/chat-interface/chat_server.py
@@ -1204,9 +1204,12 @@ CHAT_HTML = """
       </div>
     </div>
     <div class="header-actions">
-      <a href="/discussion" class="icon-btn" title="Discussion Mode">
+      <a href="/discussion" class="icon-btn" title="Switch to Roundtable Mode">
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-          <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+          <polyline points="17 1 21 5 17 9"></polyline>
+          <path d="M3 11V9a4 4 0 0 1 4-4h14"></path>
+          <polyline points="7 23 3 19 7 15"></polyline>
+          <path d="M21 13v2a4 4 0 0 1-4 4H3"></path>
         </svg>
       </a>
       <button id="themeToggle" class="icon-btn" title="Toggle Theme">
