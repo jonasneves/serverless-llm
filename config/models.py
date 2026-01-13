bc62e46a8b93aca718c376d61f1512a83943a17f
Jonas Neves <72263638+jonasneves@users.noreply.github.com>
2026-01-12 19:00:57 -0500

    Update

--- config/models.py
+++ config/models.py
@@ -368,8 +368,16 @@ if __name__ == "__main__":
     import json
 
     if len(sys.argv) > 1:
+        arg = sys.argv[1]
+
+        # Special command: --inference-dirs outputs JSON array of inference directories
+        if arg == "--inference-dirs":
+            dirs = [m.inference_dir for m in get_inference_models()]
+            print(json.dumps(dirs))
+            sys.exit(0)
+
         # CLI mode: python config/models.py <model_name> [field]
-        model_name = sys.argv[1]
+        model_name = arg
         try:
             m = get_model(model_name)
             if len(sys.argv) > 2:
